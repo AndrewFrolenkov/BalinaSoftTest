41b83ac4515488911d6280c1cbbdaedb23bafacf
Andrew Frolenkov <andrewissuess@yahoo.com>
2023-09-24 18:21:53 +0300

    Setting tableView to get data from the network
    1.Creating activity indicator for the user while the data is loading
    2.Creating activity indicator for footer tableView to load other pages from the network
    3.Creating AlertController to notify the user when all items have been loaded

--- BalinaSoftTest/Controller/ListViewController.swift
+++ BalinaSoftTest/Controller/ListViewController.swift
@@ -13,11 +13,22 @@ class ListViewController: UIViewController {
   // MARK: - Private Property
   private lazy var tableView = UITableView()
   private lazy var activityIndicatorCenter = UIActivityIndicatorView()
+  var currentPage = 1
+  var totalPage = 0
+  var isLoadingData = false // флаг для отслеживания первоначальной загрузки
+  var isLoadingDataPage = false // Флаг для отслеживания загрузки данных
+  
+  // MARK: Model
+  var page: Page?
+  
+  
   
   // MARK: - Override Methods
   override func viewDidLoad() {
     super.viewDidLoad()
     setupView()
+    
+    fetchPhotosFromServer()
   }
 }
 
@@ -51,6 +62,14 @@ private extension ListViewController {
     tableView.delegate = self
   }
   
+  func createAlertController() {
+    let alert = UIAlertController(title: "Ошибка", message: "Все элементы загружены", preferredStyle: .alert)
+    
+    let alertAction = UIAlertAction(title: "Ок", style: .default)
+    alert.addAction(alertAction)
+    self.present(alert, animated: true)
+  }
+
   //  show/hide ActivityIndicator
   func showActivityIndicator() {
     let activityIndicator = UIActivityIndicatorView(style: .medium)
@@ -81,19 +100,82 @@ private extension ListViewController {
   }
 }
 
+// MARK: - Networking
+extension ListViewController {
+  func fetchPhotosFromServer() {
+    tableView.tableFooterView = nil
+    activityIndicatorCenter.startAnimating()
+    NetworkService.shared.fetchPhotoTypes(page: 0) { [weak self] result in
+      switch result {
+      case .success(let page):
+        // Обновите массив photos с данными о фотографиях
+        self?.page = page
+        
+        // Обновите UITableView на основе полученных данных
+        DispatchQueue.main.async {
+          self?.currentPage = page.totalPages - page.totalPages + 1
+          self?.totalPage = page.totalPages
+          self?.isLoadingData = true
+          self?.activityIndicatorCenter.stopAnimating()
+          self?.tableView.reloadData()
+          
+        }
+      case .failure(let error):
+        // Обработка ошибки, например, показав сообщение об ошибке пользователю
+        print("Error fetching photos: \(error)")
+      }
+    }
+  }
+}
+
 // MARK: - UITableViewDelegate, UITableViewDataSource
 extension ListViewController: UITableViewDelegate, UITableViewDataSource {
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 10
+    return page?.content.count ?? 0
   }
   
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let page = page?.content[indexPath.row]
     let cell = tableView.dequeueReusableCell(withIdentifier: CustomCell.reuseId, for: indexPath) as! CustomCell
-    cell.nameLabel.text = "\(indexPath.row)"
+    cell.nameLabel.text = page?.name
     return cell
   }
   
   func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
     return 100.0
+  }
+  
+  func scrollViewDidScroll(_ scrollView: UIScrollView) {
+    // Проверяем, достиг ли пользователь конца таблицы
+    let offsetY = scrollView.contentOffset.y
+    let contentHeight = scrollView.contentSize.height
+    let tableHeight = scrollView.frame.height
+    
+    if isLoadingData {
+      if !isLoadingDataPage && offsetY + tableHeight > contentHeight {
+        isLoadingDataPage = true
+        showActivityIndicator()
+        if currentPage == totalPage {
+          hideActivityIndicator()
+          createAlertController()
+        } else {
+          NetworkService.shared.fetchPhotoTypes(page: currentPage) { [weak self] result in
+            switch result {
+            case .success(let page):
+              self?.currentPage += 1
+              self?.page?.content.append(contentsOf: page.content)
+              DispatchQueue.main.async {
+                self?.tableView.reloadData()
+                self?.hideActivityIndicator()
+                self?.isLoadingDataPage = false // Снимаем флаг после завершения загрузки
+              }
+            case .failure(let error):
+              print(error.localizedDescription)
+              self?.isLoadingDataPage = false // Снимаем флаг при ошибке
+            }
+          }
+        }
+      }
+    }
   }
 }
