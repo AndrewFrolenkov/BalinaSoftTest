86fd9734a23d77b986d4b4da31460f61008b00c2
Andrew Frolenkov <andrewissuess@yahoo.com>
2023-09-24 17:21:29 +0300

    project initialization

---
+++ BalinaSoftTestUITests/BalinaSoftTestUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BalinaSoftTestUITestsLaunchTests.swift
+//  BalinaSoftTestUITests
+//
+//  Created by Андрей Фроленков on 24.09.23.
+//
+
+import XCTest
+
+final class BalinaSoftTestUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
